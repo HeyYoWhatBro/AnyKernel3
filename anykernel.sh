ce4f5d9ae221d9edbcff6f255132c0692977a753
nuuwy0 <69439251+nuuwy0@users.noreply.github.com>
2023-07-28 12:01:43 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Pabji Kernel by nuuwy0 
+kernel.string=Atomic! Kernel by nuuwy0 
 do.devicecheck=1
 do.modules=0
 do.systemless=1
